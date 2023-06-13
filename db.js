339cc5d3e489e04ebbbc8c847fea2a73c6a07a35
PranishW <pranish2402@gmail.com>
2023-06-13 11:25:19 +0530

    db

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv')
-dotenv.config({path:'backend/.env'})
+dotenv.config()
 // "mongodb://localhost:27017/eRegistration"
 const MongoURI = process.env.MONGO_URI
 const connectToMongo = () =>{
     mongoose.connect(MongoURI,()=>{
         console.log("Connected to Mongoose")
+        console.log(process.env)
     });
 }
 module.exports= connectToMongo;
--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const express = require('express');
 var cors = require('cors')
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv')
-dotenv.config({path:'backend/.env'})
+dotenv.config()
 connectToMongo();
 const app = express()
-
+const port = process.env.PORT
+const BASE_URL = process.env.BASE_URL;
 app.use(cors())
 app.use(bodyParser.json({limit:'50mb'}));
   app.use(bodyParser.urlencoded({
@@ -15,6 +16,6 @@ app.use(bodyParser.json({limit:'50mb'}));
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/form',require('./routes/form'));
 
-app.listen(process.env.PORT, () => {
-    console.log(`Registration Form backend listening on port ${process.env.PORT}`)
+app.listen(port, () => {
+    console.log(`Registration Form backend listening on ${BASE_URL}`)
   })
